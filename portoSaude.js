eb1671e19d297e9ceddbf70a033dcd1084b2ef89
DiegoFSantos88 <diegofsccp@gmail.com>
2023-07-26 19:06:09 -0300

    feat:alimentacao-dos-modulos

---
+++ portoSaude.js
@@ -0,0 +1,90 @@
+export const portoSaude = {
+
+questao1:{     
+pergunta:"É possível incluir dependentes no Porto Saúde?",
+respostaA:"Isso irá depender da política de benefícios da empresa contratante.", /*respota correta*/
+respostaB:"No Porto Saúde, é possível incluir dependentes de outras galáxias.",
+respostaC:"Além dos dependentes tradicionais, o Porto Saúde permite a inclusão de animais de estimação como dependentes.",
+respostaD: "No Porto Saúde, é possível incluir dependentes imaginários."
+},
+
+questao2:{
+pergunta:"Como funciona a coparticipação?",
+respostaA:"A coparticipação é um sistema de recompensa, onde o beneficiário recebe pontos a cada vez que utiliza o plano de saúde.",
+respostaB:"Na coparticipação, o beneficiário não precisa pagar nada além da mensalidade.", 
+respostaC:"A coparticipação é o valor pago por quem contrata o seguro seja pessoa física ou jurídica, para um grupo de pessoas e/ou pelo segurado.", /*respota correta*/
+respostaD:"A coparticipação é um sistema de economia colaborativa." 
+},
+
+questao3:{
+pergunta:"Os planos da Porto Saúde e Odontológico dão direito ao Reembolso?",
+respostaA:"Nos planos da Porto Saúde e Odontológico, o reembolso é dado em forma de créditos para serem utilizados em compras de chocolates e doces.",
+respostaB:"Com exceção da Linha Pro, os demais planos de saúde e odontológico dão direito ao reembolso.", /*respota correta*/
+respostaC:"A Porto Saúde e Odontológico oferecem o reembolso em forma de passagens aéreas para destinos paradisíacos.", 
+respostaD:"Os planos da Porto Saúde e Odontológico oferecem um reembolso em forma de obras de arte." 
+},
+
+
+questao4:{
+pergunta:"Como funcionam os Programas de Promoção da Saúde?", 
+respostaA:"Os programas de promoção da saúde funcionam como uma espécie de roleta de hábitos.",
+respostaB:"Os Programas são realizados dentro das unidades Portomed e o tratamento é feito por equipes especializadas no assunto.", /*respota correta*/
+respostaC:"Em vez de enfatizar a adoção de hábitos saudáveis, os programas de promoção da saúde são uma espécie de dia livre para os participantes.", 
+respostaD:"Os Programas de Promoção da Saúde consistem em uma série de desafios divertidos." 
+},
+
+
+questao5:{
+pergunta:"O que é CIPA? É obrigatória?",
+respostaA:"A sigla CIPA significa Comissão Interna de Prevenção de Acidentes e é composta por um grupo de representantes da empresa.", /*respota correta*/
+respostaB:"A CIPA é uma comissão secreta de agentes secretos da empresa, responsáveis por investigar e combater atividades clandestinas no ambiente de trabalho.",
+respostaC:"CIPA é uma comissão de entretenimento da empresa, responsável por organizar festas, eventos e atividades recreativas para os funcionários.",
+respostaD:"CIPA é uma comissão de interpretação artística, encarregada de encenar peças teatrais sobre prevenção de acidentes no trabalho."
+},
+
+
+questao6:{
+pergunta:"Quem pode contratar o Seguro Odontológico?",
+respostaA:"Apenas unicórnios falantes podem contratar o Seguro Odontológico.", 
+respostaB:"Apenas os fãs de sorrisos engraçados têm acesso ao Seguro Odontológico.", 
+respostaC:"O Seguro Odontológico é reservado apenas para pessoas que tenham sorrido para a Lua cheia pelo menos uma vez em suas vidas.", 
+respostaD:"O seguro pode ser contratado por empresas a partir de 03 vidas em todo o território nacional." /*respota correta*/
+},
+
+
+questao7:{
+pergunta:"O Seguro Odontológico tem cobertura para tratamento de canal?",
+respostaA:"O Seguro Odontológico oferece cobertura de tratamento de canal para dentes que precisam ser curados de cáries cantantes.",
+respostaB:"O Seguro Odontológico oferece cobertura de tratamento de canal para dentes que estão sorrindo o tempo todo.",
+respostaC:"Sim. Você encontra a lista de dentistas disponíveis, acessando a página de Rede Referenciada Odonto.",  /*respota correta*/
+respostaD:"No Seguro Odontológico, o tratamento de canal é coberto somente para dentes que precisam de um tratamento musical." 
+},
+
+
+questao8:{
+pergunta:"Qual é a vigência do seguro odontológico?",
+respostaA:"A vigência da apólice será de 12 ou 24 meses, contados a partir da formalização da contratação.",  /*respota correta*/
+respostaB:"A vigência do seguro odontológico é contada em estações do ano.", 
+respostaC:"A vigência do seguro odontológico é medida em ciclos lunares.",  
+respostaD:"O seguro odontológico possui uma vigência determinada por enigmas matemáticos." 
+},
+
+
+questao9:{
+pergunta:"Onde consigo o número da carteirinha?",
+respostaA:"O número da carteirinha está escondido em um labirinto encantado no fundo do mar.",
+respostaB:"O número da carteirinha pode ser acessado por meio de um portal dimensional.",
+respostaC:"Você pode consultar o número da carteirinha do seu plano Seguro Saúde e/ou Plano Odontológico por meio do aplicativo ou da Área do Cliente.", /*respota correta*/
+respostaD:"O número da carteirinha é guardado por uma árvore sábia em uma floresta encantada."
+},
+
+
+questao10:{
+pergunta:"O segurado pode escolher um dentista fora da Rede Referenciada?",
+respostaA:"Sim, o segurado pode escolher um dentista fora da Rede Referenciada, mas ele deve desafiar o dentista em uma partida de xadrez.", 
+respostaB:"Sim, os segurados têm liberdade para escolher o dentista de sua preferência. Nesse caso, é feito o reembolso das despesas realizadas.", /*respota correta*/
+respostaC:"O segurado pode escolher um dentista fora da Rede Referenciada, mas somente se ele trouxer um buquê de flores encantadas para o dentista.", 
+respostaD:"O segurado pode escolher um dentista fora da Rede Referenciada, mas somente se ele conseguir encontrar uma chave escondida em um labirinto."
+}
+
+};
